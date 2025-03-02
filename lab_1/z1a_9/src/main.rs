bc1a2f59308e992b5792be2641dab7725dd27a5f
nats <139819517+natsloo@users.noreply.github.com>
2025-03-02 20:11:07 +0100

    brakujący średnik

--- lab_1/z1a_9/src/main.rs
+++ lab_1/z1a_9/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let dana = 25;
+    let dana = 100;
 
     for a in 1..dana {
         for b in a + 1..dana {
             for c in b + 1..=dana {
                 //println!("{a}, {b}, {c}")
                 if a * a + b * b == c * c {
-                    println!("({a}, {b}, {c})")
+                    println!("({a}, {b}, {c})");
                 }
             }
         }

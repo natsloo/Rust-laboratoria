8f7e156ac0338a6c7b50e7a99a4232c0123a8a7d
nats <139819517+natsloo@users.noreply.github.com>
2025-03-29 19:34:05 +0100

    lab 5 cont.

--- lab_5/z5b/z5b_3/src/main.rs
+++ lab_5/z5b/z5b_3/src/main.rs
@@ -1,3 +1,17 @@
+fn wartosc_cyfry_rzymskiej(c: char) -> Result<u16, String> {
+    let symbols = [('I', 1), ('V',5), ('X', 10),
+        ('L', 50), ('C', 100), ('D', 500), ('M', 1000)];
+    for (string, value) in symbols {
+        if c == string {
+            return Ok(value)
+        }
+    }
+    Err("Znak nie jest cyfrą rzymską!".to_string())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match wartosc_cyfry_rzymskiej('-') {
+        Ok(l) => println!("{}",l),
+        Err(e) => println!("{}",e)
+    }
 }
--- lab_5/z5b/z5b_4/src/main.rs
+++ lab_5/z5b/z5b_4/src/main.rs
@@ -1,3 +1,56 @@
+fn wartosc_cyfry_rzymskiej(c: char) -> Result<u16, String> {
+    let symbols = [('I', 1), ('V',5), ('X', 10),
+        ('L', 50), ('C', 100), ('D', 500), ('M', 1000)];
+    for (string, value) in symbols {
+        if c == string {
+            return Ok(value)
+        }
+    }
+    Err("Znak nie jest cyfrą rzymską!".to_string())
+}
+
+fn rzymskie(napis: &str) -> Result<u128, String> {
+    let znaki:Vec<char> = napis.chars().rev().collect();
+
+    let mut poprzedni = 0;
+    let mut wynik = 0;
+    let mut licznik = 1;
+
+    for (i,&c) in znaki.iter().enumerate() {
+       let w = wartosc_cyfry_rzymskiej(c)? as u128;
+
+        if i > 0 && w == poprzedni {
+            licznik += 1;
+
+            if c == 'V' || c == 'L' || c == 'D' {
+                return Err(format!("Znak {} nie może się powtarzać!", c));
+            }
+
+            if licznik > 3 {
+                return Err(format!("Za dużo powtórzeń znaku {}!",c));
+            }
+        }
+        else {
+            licznik = 1;
+        }
+
+        if w >= poprzedni {
+            wynik += w;
+        }
+        else {
+            if poprzedni > 10 * w {
+                return Err("Niewłaściwa kolejność cyfr!".to_string())
+            }
+            wynik -= w;
+        }
+        poprzedni = w;
+    }
+    Ok(wynik)
+}
+
 fn main() {
-    println!("Hello, world!");
+    match rzymskie("MMMCDL") {
+        Ok(l) => println!("{}", l),
+        Err(e) => println!("{}", e),
+    }
 }
